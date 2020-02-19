93672c99490da7221bc673f730a166ba12622a7e
raul <raulhabits@gmail.com>
2020-02-19 00:36:35 -0500

    Updates

---
+++ precache-manifest.60fc1991dac47e58d999b7d0a9382c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7066919ea6c0a70022bf0989c17fbd1e",
+    "url": "/adidas/index.html"
+  },
+  {
+    "revision": "ac210a81c52a98a7ab83",
+    "url": "/adidas/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "49b568f0ddddc0b0bd67",
+    "url": "/adidas/static/js/2.433a194a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/adidas/static/js/2.433a194a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac210a81c52a98a7ab83",
+    "url": "/adidas/static/js/main.29728cc0.chunk.js"
+  },
+  {
+    "revision": "227285abdaac64c4ef2d",
+    "url": "/adidas/static/js/runtime-main.a391f773.js"
+  }
+]);
