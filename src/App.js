e7c7a09d2f13a74822a911d2d6fabc0867d7b8bc
Jairo Pinzon <jairo.pinzon@avaldigitallabs.com>
2020-02-19 00:25:16 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header></header>
+     <main></main>
+    <footer></footer>
+    </div>
+  );
+}
+
+export default App;
